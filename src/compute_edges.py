bdcccf8ca81140af5d94b48b99884af2bc03dc6c
Lawrence Pang <lawrencepang36@gmail.com>
2018-04-14 23:22:52 -0400

    visualization changes

--- src/compute_edges.py
+++ src/compute_edges.py
@@ -7,9 +7,9 @@ from copy import copy
 import random
 import math
 import numpy
-from matplotlib import pylab
+from matplotlib import pylab, colors
 
-def compute_edges():
+def compute_edges(target):
   kws = []
   with open('../data/keywords.pkl','rb') as f:
     kws = pickle.load(f)
@@ -21,6 +21,8 @@ def compute_edges():
   embed = []
   with open('../data/2dembed.pkl','rb') as f:
     embed = pickle.load(f)
+  if target in kws_table:
+    embed[kws_table[target]] = [sum([a for a,_ in embed])/len(embed),sum([a for _,a in embed])/len(embed)]
   
   data = []
   with open('../data/entitydata.pkl','rb') as f:
@@ -36,18 +38,19 @@ def compute_edges():
         else:
           edges[(temp[i],temp[j])] = 1
   
-  def plot(embeddings, labels, edges):
+  def plot(embeddings, labels, edges, keywords):
     assert len(embeddings) >= len(labels), 'More labels than embeddings'
     pylab.figure(figsize=(15,15))	# in inches
     for i, label in enumerate(labels):
       x, y = embeddings[i]
-      pylab.scatter(x, y)
+      pylab.scatter(x, y, s=keywords[i][0], c='blue')
       pylab.annotate(label, xy=(x, y), xytext=(5, 2), textcoords='offset points',
                      ha='right', va='bottom')
+    limit = max(edges.values())+0.0
     for i in edges:
-      pylab.plot([embeddings[i[0]][0],embeddings[i[1]][0]],[embeddings[i[0]][1],embeddings[i[1]][1]],lw=math.log(edges[i]))
+      pylab.plot([embeddings[i[0]][0],embeddings[i[1]][0]],[embeddings[i[0]][1],embeddings[i[1]][1]],lw=math.log(edges[i]+1),c=(1,0,0,edges[i]/limit))
     pylab.show()
 
-  plot(embed, [a[1] for a in kws], edges)
+  plot(embed, [a[1] for a in kws], edges, kws)
   
-compute_edges()
+compute_edges("donald trump")
