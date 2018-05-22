5abea8d0a5212caa4e1649fd92861c157d4dbbeb
Lawrence Pang <lawrencepang36@gmail.com>
2018-05-21 22:16:28 -0400

    bug fixes

--- aws/write-lambda/write.py
+++ aws/write-lambda/write.py
@@ -5,7 +5,7 @@ def write(event,context):
   import os
   import time
   import datetime
-  from copy import copy
+  from copy import deepcopy
   #import nltk
   from nltk import pos_tag,ne_chunk,word_tokenize
   from nltk.data import load
@@ -72,7 +72,19 @@ def write(event,context):
       tags.append(proper_noun_tag(sentence))
   #print 'done sentence tagging'
   
+  # terms: array of terms like ([default,term1,term2...],count)
+  # term_to_ind: dictionary of terms like term: index
+  # default_terms: array of default terms
+  # temp: same as terms but with changes made
+  def make_reverse(arr):
+    output = {}
+    for i,a in enumerate(arr):
+      for term in a[0]:
+        output[term] = i
+    return output
+  
   terms = load_from('terms.txt')
+  term_to_ind = make_reverse(terms)
   new_terms = {}
   
   count = len(terms)
@@ -80,12 +92,18 @@ def write(event,context):
   for tag in tags:
     for i in tag.subtrees(filter=lambda x: x.label()=='NE'):
       name = regex.sub('',str(i.flatten())[4:-1]).lower()
-      if name in new_terms:
+      if name in term_to_ind:
+        terms[term_to_ind[name]][1]+=1
+      elif name in new_terms:
         new_terms[name]+=1
       else:
         new_terms[name] = 1
   terms.extend([[name],new_terms[name]] for name in new_terms)
   #add nnp leaves
+              
+  #if len(terms)>100: #heuristic for eliminating terms
+  terms = [v for v in terms if v[1]>1]      
+  term_to_ind = make_reverse(terms)
     
   exceptions = []
   targets = []
@@ -106,19 +124,6 @@ def write(event,context):
   with open('./data/words.txt','r') as f:
     for line in f:
       word_names.append(line.replace('\n',''))
-                                     
-  # terms: array of terms like ([default,term1,term2...],count)
-  # term_to_ind: dictionary of terms like term: index
-  # default_terms: array of default terms
-  # temp: same as terms but with changes made
-  def make_reverse(arr):
-    output = {}
-    for i,a in enumerate(arr):
-      for term in a[0]:
-        output[term] = i
-    return output
-  term_to_ind = make_reverse(terms)
-  temp = copy(terms)
   
   # D: array like terms
   # K: index
@@ -126,12 +131,13 @@ def write(event,context):
   # we always use the earlier index for backwards compatibility
   # syntax is merging K (source) into T (target)
   def merge(D,K,T):
-    if not D[K] or not D[T]:
+    if not D[K] or not D[T] or K==T:
       return
     D[T][1]+=D[K][1]
     D[T][0].extend(D[K][0])
     D[K] = False
   
+  temp = deepcopy(terms)
   #check common endings
   for t in term_to_ind:
     if len(t)>4 and (t[-1]=='n' or t[-1]=='s'):
@@ -140,7 +146,7 @@ def write(event,context):
           merge(temp,term_to_ind[t],term_to_ind[t[:-i]])
           break
   terms = [t for t in temp if t]
-  temp = copy(terms)
+  temp = deepcopy(terms)
   term_to_ind = make_reverse(terms)
                                      
   #check blacklist
@@ -152,21 +158,29 @@ def write(event,context):
   #check names (first and last)
   terms = sorted([(i[1],i[0]) for i in terms],reverse=True)
   terms = [[i[1],i[0]] for i in terms] #convert back to list
-  temp = copy(terms)
+  temp = deepcopy(terms)
   for i,temp_list in enumerate(terms):
     t_list = temp_list[0]
     for t in t_list:
       if len(t.split(' '))==1:
-        for j in range(i):
+        flag = False
+        for j in range(len(terms)):
           for s in terms[j][0]:
-            if t!=s and len(s)>len(t) and s.rfind(t)==len(s)-len(t):
+            if i!=j and t!=s and len(s.split(' '))==2 and s.split(' ')[1]==t:
               merge(temp,i,j)
+              flag = True
               break
-            elif t!=s and len(s.split(' '))==2 and s.split(' ')[0]==t:
+            #removing first names for now...
+            elif i!=j and t!=s and len(s.split(' '))==2 and s.split(' ')[0]==t:
               merge(temp,i,j)
+              flag = True
               break
+          if flag:
+            break
+        if flag:
+          break
   terms = [t for t in temp if t]
-  temp = copy(terms)
+  temp = deepcopy(terms)
   term_to_ind = make_reverse(terms)
   
   #check exception list
@@ -186,7 +200,7 @@ def write(event,context):
         merge(temp,term_to_ind[t],target)
         break
   terms = [t for t in temp if t]
-  temp = copy(terms)
+  temp = deepcopy(terms)
   term_to_ind = make_reverse(terms)
 
   #check common words
@@ -230,9 +244,6 @@ def write(event,context):
         a['keywords'][i] = needs_updating[kw]
       if kw not in term_to_ind:
         del a['keywords'][i]
-        
-  #if len(terms)>100: #heuristic for eliminating terms
-  terms = [v for v in terms if v[1]>min(len(terms)/100,5)]
    
   save_to(terms,'terms.txt')
   save_to(existing_articles,'articles.txt')
