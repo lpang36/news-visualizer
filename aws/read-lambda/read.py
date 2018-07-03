cdfa455cd4afff1b8f880e7731c70abf75831e16
Lawrence Pang <lawrencepang36@gmail.com>
2018-07-02 20:25:57 -0400

    integrate elasticsearch with read lambda

--- aws/read-lambda/read.py
+++ aws/read-lambda/read.py
@@ -1,33 +1,72 @@
+import json
+from elasticsearch import Elasticsearch,RequestsHttpConnection,helpers
+
+BONSAI_URL = 'https://ezq74z6t3a:gc0wwgwdvp@news-visualizer-2976423464.us-east-1.bonsaisearch.net'
+ITEMS_PER_DOC = 4
+
+def connectES(esEndPoint):
+  print ('Connecting to the ES Endpoint {0}'.format(esEndPoint))
+  try:
+    esClient = Elasticsearch(
+    hosts=[{'host': esEndPoint, 'port': 443}],
+    use_ssl=True,
+    verify_certs=True,
+    connection_class=RequestsHttpConnection)
+    return esClient
+  except Exception as E:
+    print("Unable to connect to {0}".format(esEndPoint))
+    print(E)
+    exit(3)
+es = connectES(BONSAI_URL)
+
 def read(event,context):  
-  import json
-  from s3_interface import load_from,save_to
+  event['q'] = event['q'].lower()
+  should_match  = []
+  for i in range(ITEMS_PER_DOC):
+    for j in ['description','title']:
+      should_match.append({
+        'match': {
+            j+'_'+str(i): {
+              'query':event['q'],
+              '_name':str(i)
+            }
+        }
+      })
+  query = {
+    'query': {
+      'bool': {
+        'should': should_match
+      }
+    }
+  }
+  articles = es.search(index='news-visualizer',doc_type='article',body=query,size=10000)['hits']['hits']
   
-  articles = load_from('articles.txt')
-  
-  def process_article(article):
-    return {'title':article['title'],'time':article['time'],'url':article['url']}
+  def process_article(article,ind):
+    return {'title':article['title_'+str(ind)],'time':article['time_'+str(ind)],'url':article['url_'+str(ind)]}
   
   ### compute edges ###
   edges = {}
   nodes = {}
-  event['q'] = event['q'].lower()
   #sentiments = {}
   for a in articles:
-    if event['q'] in a['title'].lower() or event['q'] in a['description'].lower(): #needs refining
-      temp = sorted(a['keywords'])
-      for i in range(len(temp)):
-        for j in range(i+1,len(temp)):
-          if (temp[i],temp[j]) in edges:
-            edges[(temp[i],temp[j])]+=1
-            #sentiments[(temp[i],temp[j])]+=articles[inds[k]]['sentiment']
+    matched = [int(num) for num in a['matched_queries']]
+    a = a['_source']
+    for ind in range(ITEMS_PER_DOC):
+      if ind in matched:
+        temp = sorted(ast.literal_eval(a['keywords_'+str(ind)]))
+        for i in range(len(temp)):
+          for j in range(i+1,len(temp)):
+            if (temp[i],temp[j]) in edges:
+              edges[(temp[i],temp[j])]+=1
+              #sentiments[(temp[i],temp[j])]+=articles[inds[k]]['sentiment']
+            else:
+              edges[(temp[i],temp[j])] = 1
+              #sentiments[(temp[i],temp[j])] = articles[inds[k]]['sentiment']
+          if temp[i] in nodes:
+            nodes[temp[i]]['size']+=1
+            nodes[temp[i]]['articles'].append(process_article(a))
           else:
-            edges[(temp[i],temp[j])] = 1
-            #sentiments[(temp[i],temp[j])] = articles[inds[k]]['sentiment']
-        if temp[i] in nodes:
-          nodes[temp[i]]['size']+=1
-          nodes[temp[i]]['articles'].append(process_article(a))
-        else:
-          nodes[temp[i]] = {'id':temp[i],'label':temp[i],'size':1,'articles':[process_article(a)]}
+            nodes[temp[i]] = {'id':temp[i],'label':temp[i],'size':1,'articles':[process_article(a,ind)]}
   
   ### output json ###
   output = {} #beware of duplicate name
@@ -41,9 +80,9 @@ def read(event,context):
       'value': edges[edge]
     })
     
-  with open('./temp/graph.json','wb') as f:
-    json.dump(output,f)
+  #with open('./temp/graph.json','wb') as f:
+  #  json.dump(output,f)
   
   return output
 
-read({'q':'trump'},None)
+#read({'q':'trump'},None)
